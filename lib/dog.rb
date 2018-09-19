a6429d0ea880c61a16d52db085317f03a74d751e
Jenny K <jkam17@gmail.com>
2018-09-19 17:37:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,4 +21,24 @@ attr_accessor :id, :name, :breed
     DB[:conn].execute('drop table if exists dogs')
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id,name,breed) 
+  end 
+
+  def self.find_by_name(name)
+    sql = "select * from dogs where name = ?"
+    self.new_from_db(DB[:conn].execute(sql,name).first)
+  end 
+
+  def update
+
+  end 
+
+  def save 
+
+  end
+
 end
