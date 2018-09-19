a395f524034c2fce8280c1227a6c6e04fe1c5690
Jenny K <jkam17@gmail.com>
2018-09-19 17:42:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,15 +39,15 @@ attr_accessor :id, :name, :breed
   end
 
   def save
-    if self.id 
-      self.update 
-    else 
-      sql <<-SQL 
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
         insert into dogs(name, breed)
-        values (?, ?)   
+        values (?, ?)
         SQL
       DB[:conn].execute(sql,self.name, self.breed)
-    end 
+    end
   end
 
 end
